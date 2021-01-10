dbf61898e60aca5e054853f70ee2ca5244783961
francisgodinho <francisgodinho2010@hotmail.com>
2021-01-09 18:09:40 -0800

    trying to merge this thing

--- frontend/noticate/src/App.js
+++ frontend/noticate/src/App.js
@@ -4,20 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>File Upload</h1>
+
+      <form action="http://localhost:9000/upload" method="POST" enctype="multipart/form-data">
+        <input type="file" name="avatar"></input>
+        <button>Submit</button>
+      </form>
+
     </div>
   );
 }
