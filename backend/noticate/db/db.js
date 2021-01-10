9e0228569d5c7171b53cad7fdd61de62557fc306
briellelaw <lawbrielle@gmail.com>
2021-01-09 22:02:59 -0800

    Connects to db when starting server

---
+++ backend/noticate/db/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const connOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
+const connStr = "mongodb://localhost:27017/noticate";
+//const connStr = "mongodb://0.0.0.0:27017/noticate";
+
+// Connect to the local databse
+mongoose.connect(connStr, connOptions);
+mongoose.Promise = global.Promise;
+
+// Get the user schemas
+const User = require('./usermodel.js');
+
+module.exports = {
+	User
+};
