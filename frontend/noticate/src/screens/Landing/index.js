1d6e27e76abc719a028753a099d39649a6c4a3f8
emilyl <emily.lukas@gmail.com>
2021-01-09 16:10:36 -0800

    add home screen styling

--- frontend/noticate/src/screens/Landing/index.js
+++ frontend/noticate/src/screens/Landing/index.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Logo from '../../assets/LandingBooks.png';
+import Blob1 from '../../assets/Component1.png';
+import Blob2 from '../../assets/Component2.png';
+import Blob3 from '../../assets/Component3.png';
 import { GoogleLogin } from 'react-google-login';
 
 function Landing() {
@@ -14,6 +18,18 @@ function Landing() {
 
   return (
     <div>
+      <img src = {Blob1} alt="blob1" style={{ position: 'absolute' }}/>
+      <img src = {Blob2} alt="blob2" style={{ position: 'absolute', left: '50%', marginTop: '38.5%', marginBottom: 0 }}/>
+      <img src = {Blob3} alt="blob3" style={{ position: 'absolute', right: '0%' }} />
+
+      <div style={{ position: 'absolute', top: '10%', left: '40%', textAlign: 'center' }} >
+        <img src = {Logo} alt="logo" style={{}} />
+        <h1 style = {{marginTop: -30}}>Noticate</h1>
+        <p>This is some text .</p>
+      </div>
+
+
+
       <p>Landing Page</p>
       <GoogleLogin
         clientId='1072009986202-bj557860kagjt30ti31vcfgf8heeoprg.apps.googleusercontent.com'
