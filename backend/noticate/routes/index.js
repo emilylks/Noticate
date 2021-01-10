0ac15edfbddbfbd28e708e39aa23fe507d035d21
Ubuntu <ubuntu@ip-172-31-38-89.us-east-2.compute.internal>
2021-01-10 03:02:18 +0000

    from server

--- backend/noticate/routes/index.js
+++ backend/noticate/routes/index.js
@@ -4,6 +4,8 @@ var router = express.Router();
 const multer = require("multer"); 
 const uuid = require("uuid").v4;
 
+var mongodb = require('mongodb');
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
       cb(null, 'uploads');
@@ -25,6 +27,33 @@ router.use(express.static('public'));
 
 router.post("/upload", upload.single('avatar'), (req, res) => {
     return res.json({status: "OK"});
+});
+
+router.get('/thelist', function(req, res){
+	var MongoClient = mongodb.MongoClient;
+	var url = 'mongodb://ec2-18-222-146-206.us-east-2.compute.amazonaws.com:27017/sampsite';
+	MongoClient.connect(url, function(err, db){
+		if(err){
+			console.log("unable to connect", err);
+		}
+		else{
+			console.log("connection established");
+			var collection = db.collection('students');
+			collection.find({}).toArray(function(err, result){
+				if(err){
+					res.send(err);
+				}
+				else if(result.length){
+					res.render('studentlist', {
+						"studentList": result;
+					})
+				}
+				else
+					res.send("no documents found");
+			db.close();
+			});
+		}
+	});
 });
 
 module.exports = router;
